2878d95a2f8f62d83182963e7d6fd5ab75711ad3
jalehoux <jalehoux@gmail.com>
2018-08-06 19:10:34 -0400

    added auth stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var url = window.location.href;
   var two = url.split("/");
   var three = two[two.length - 1];
@@ -21,9 +21,10 @@ $(document).ready(function() {
   var uid = "";
 
   //firebase user stuff
-  firebase.auth().onAuthStateChanged(function(user) {
+  firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       uid = user.uid;
+      $('.navAccountButton').html(user.displayName);
       getTasks(user.uid);
     }
     if (user && three == "index.html") {
@@ -31,76 +32,76 @@ $(document).ready(function() {
     }
   });
 
-    //meetup api
-    jQuery.ajaxPrefilter(function (options) {
-        if (options.crossDomain && jQuery.support.cors) {
-            options.url = 'https://cryptic-headland-94862.herokuapp.com/' + options.url;
-        }
+  //meetup api
+  jQuery.ajaxPrefilter(function (options) {
+    if (options.crossDomain && jQuery.support.cors) {
+      options.url = 'https://cryptic-headland-94862.herokuapp.com/' + options.url;
+    }
 
-    });
+  });
 
+  var meetupKey = "421f4e447d7a4c403e52346147257a50";
+  var userZip = $("#userZip").val();
+  var meetupURL = `https://api.meetup.com/find/groups?category=10&sign=true&key=${meetupKey}&zip=${userZip}&radius=1&event`;
 
-    var meetupKey = "421f4e447d7a4c403e52346147257a50";
-    var userZip = $("#userZip").val();
-    var meetupURL = `https://api.meetup.com/find/groups?category=10&sign=true&key=${meetupKey}&zip=${userZip}&radius=1&event`;
+  // AJAX call
 
-    // AJAX call
+  $.ajax({
+    url: meetupURL,
+    method: "GET"
 
-    $.ajax({
-        url: meetupURL,
-        method: "GET"
+  }).then(function (response) {
 
-    }).then(function (response) {
+    console.log(response[0].name);
+    console.log(response[0].description);
+    console.log(response[0].city);
+    console.log(response[0].next_event);
+  })
 
-        console.log(response[0].name);
-        console.log(response[0].description);
-        console.log(response[0].city);
-        console.log(response[0].next_event);
-    })
-
-    $('#signUpButton').on("click", function() {
-      var email = $("#signUpEmail").val();
-      var password = $("#signUpPassword").val();
-      var fullName = $("#first_name").val() + " " + $("#last_name").val();
+  $('#signUpButton').on("click", function () {
+    var email = $("#signUpEmail").val();
+    var password = $("#signUpPassword").val();
+    var fullName = $("#first_name").val() + " " + $("#last_name").val();
+    var confirm = $("#confirmPassword").val();
+    if (password != confirm) {
+      M.toast({ html: "Passwords don't match!" });
+    } else {
       firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(user => {
-          user.updateProfile({
+        .then(() => {
+          firebase.auth().currentUser.updateProfile({
             displayName: fullName
-          });
+          })
         })
-
         .then(() => {
           window.location.href = "./account.html";
         })
-        .catch(function(error) {
+        .catch(function (error) {
           var errorCode = error.code;
           var errorMessage = error.message;
-          console.log(errorCode);
           M.toast({ html: errorMessage });
-          console.log(errorMessage);
         });
-        
-    });
+    }
+  });
 
-    $('#loginButton').on("click", function () {
-        var email = $('#email').val();
-        var password = $('#password').val();
-        firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
-            window.location.href = "./account"
-        }).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            M.toast({html: errorMessage});
-        });
+  $('#loginButton').on("click", function () {
+    var email = $('#email').val();
+    var password = $('#password').val();
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
+      window.location.href = "./account.html"
+    }).catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      M.toast({ html: errorMessage });
+    });
+  })
+
+  $('.navLogoutButton').on("click", function () {
+    firebase.auth().signOut().then(function () {
+      window.location.href = "./index.html"
     })
+  });
 
-    $('.navLogoutButton').on("click", function () {
-        firebase.auth().signOut().then(function () {
-            window.location.href = "./index.html"
-        })
-    });
-
-  $("#addtask").on("click", function(event) {
+  $("#addtask").on("click", function (event) {
     event.preventDefault();
     var title = $("#title").val().trim();
     var date = $("#date").val().trim();
@@ -126,27 +127,35 @@ $(document).ready(function() {
     $("#textarea1").val("");
   });
 
-    $("#home-btn").on("click", function() {
-      document.body.scrollTop = 0; // For Safari
-      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    });
+  $("#home-btn").on("click", function () {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  });
 
   function getTasks(uid) {
-    database.ref("/tasks").orderByChild("uid").equalTo(uid).on("value", function(snapshot) {
-        $(".collapsible").empty();
-        snapshot.forEach(element => {
-          var id = element.key;
-          $(".collapsible").prepend(
-            "<li><div class='collapsible-header'><i class='material-icons'>filter_drama</i>" +
-              element.val().title + "<span class='badge' data=" + id + ">X</span></div><div class='collapsible-body'><p>" +
-              element.val().details + "</div></li>");
-        });
+    database.ref("/tasks").orderByChild("uid").equalTo(uid).on("value", function (snapshot) {
+      $(".collapsible").empty();
+      snapshot.forEach(element => {
+        var id = element.key;
+        $(".collapsible").prepend(
+          "<li><div class='collapsible-header'><i class='material-icons'>filter_drama</i>" +
+          element.val().title + "<span class='badge' data=" + id + ">X</span></div><div class='collapsible-body'><p>" +
+          element.val().details + "</div></li>");
+      });
     });
+  }
+  $(document).on('click', '.badge', function () {
+    key = $(this).attr('data')
+    database.ref('/tasks').child(key).remove();
+  });
 
-    $(document).on('click', '.badge', function(){
-        key = $(this).attr('data')
-        database.ref('/tasks').child(key).remove();
-      }); 
-
-}
+  $(document).on('click', '.navAccountButton', function () {
+    user = firebase.auth().currentUser;
+    var firstName = user.displayName.split(" ")[0];
+    var lastName = user.displayName.split(" ")[1];
+    var email = user.email;
+    $('#change_last_name').val(lastName);
+    $('#change_first_name').val(firstName);
+    $('#changeEmail').val(email);
+  });
 });       
